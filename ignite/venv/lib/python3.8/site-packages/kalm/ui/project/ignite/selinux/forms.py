4ade46d9b0d7ca11c5ee33e172916fade8137361
Jakob Holst <jho@miracle.dk>
2024-02-03 21:59:02 +0100

    demo

--- ignite/venv/lib/python3.8/site-packages/kalm/ui/project/ignite/selinux/forms.py
+++
@@ -1,8 +0,0 @@
-# forms.py
-from django import forms
-from .models import suggestion
-
-class suggestionForm(forms.ModelForm):
-    class Meta:
-        model = suggestion
-        fields = ['status']
