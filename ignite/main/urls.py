e667ad0b8012719960cdbea738afe955ea0dcdf2
Jakob Holst <jakob.holst@knowit.dk>
2024-02-08 07:50:05 +0100

    models

--- ignite/main/urls.py
+++ ignite/main/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from views import MainView
+from .views import MainView
 
 routes = routers.DefaultRouter()
 
