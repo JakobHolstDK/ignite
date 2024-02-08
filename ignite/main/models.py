fb8ea13f566da831e8176e6bfa01f8dbc6fce9f4
Jakob Holst <jakob.holst@knowit.dk>
2024-02-08 07:41:21 +0100

    models

--- ignite/main/models.py
+++ ignite/main/models.py
@@ -3,6 +3,14 @@ from django.db import models
 class metadata(models.Model):
     tenant = models.CharField(max_length=128, primary_key=True)
     logo = models
+    about = models.TextField()
+    adress = models.CharField(max_length=128)
+    phone = models.CharField(max_length=128)
+    email = models.EmailField()
+    website = models.URLField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.tenant
     
@@ -11,3 +19,36 @@ class metadata(models.Model):
         verbose_name = 'host'
         verbose_name_plural = 'hosts'
         ordering = ['hostname']
+
+class service(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        db_table = 'service'
+        verbose_name = 'service'
+        verbose_name_plural = 'services'
+        ordering = ['name']
+
+class enabled_service(models.Model):
+    tenant = models.ForeignKey(metadata, on_delete=models.CASCADE)
+    service = models.ForeignKey(service, on_delete=models.CASCADE)
+    environtent = models.JSONField(None, blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.tenant
+    
+    class Meta:
+        db_table = 'enabled_service'
+        verbose_name = 'enabled_service'
+        verbose_name_plural = 'enabled_services'
+        ordering = ['tenant']
+
+
