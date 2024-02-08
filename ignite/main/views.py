05c1d9a2ca9ac452d91310df0a41812c1f1b2f73
Jakob Holst <jakob.holst@knowit.dk>
2024-02-08 08:52:48 +0100

    models

--- ignite/main/views.py
+++ ignite/main/views.py
@@ -13,7 +13,10 @@ def MainView(request):
     pprint.pprint(request)
     # i need to print the environment variables all of them
     pprint.pprint(os.environ)
+    # i need to dump what i know about the browser doing the request
+    pprint.pprint(request.META)
     
+
 
     try:
         tenant_entries = tenant.objects.all()
