9d79da17f1dba8c2c3b0245c57cd573f2efe5a39
Jakob Holst <jakob.holst@knowit.dk>
2024-02-08 08:26:36 +0100

    models

--- ignite/main/admin.py
+++ ignite/main/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import tenant, service, enabled_service
+from .models import tenant, service, enabled_service, TutorialCategory, TutorialSeries, Tutorial
 
 admin.site.register(tenant)
 admin.site.register(service)
 admin.site.register(enabled_service)  
+admin.site.register(TutorialCategory)
+admin.site.register(TutorialSeries)
+admin.site.register(Tutorial)
+
 
 
 
