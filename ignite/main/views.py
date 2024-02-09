dd7bdb48b4eadab0c6bd16563d0cffaeabd3b95f
Jakob Holst <jho@miracle.dk>
2024-02-09 22:35:51 +0100

    demo

--- ignite/main/views.py
+++ ignite/main/views.py
@@ -2,6 +2,7 @@ import pprint
 import os
 import redis
 import json
+import hvac
 
 
 
@@ -12,6 +13,23 @@ from .models import tenant, service, enabled_service
 
 
 # Create your views here.
+def checkvault(path):
+  ign8_vault_url  = os.environ.get("VAULT_URL", "https://vault.openknowit.com")
+  ign8_vault_token = os.environ.get("VAULT_TOKEN", "s.5Y9pZ4x6y3sZ4y3s")
+  client = hvac.Client(
+    url=ign8_vault_url,
+    token=ign8_vault_token,
+  )
+  create_response = client.secrets.kv.v2.get.secret(
+    path=path,
+  )
+  if create_response.status_code == 200:
+    print("Vault secret found")
+    return True
+  else:
+    print("Vault secret not found")
+  return  False
+
 
 
 @csrf_exempt
@@ -20,8 +38,9 @@ def MainView(request):
     sessionkey = request.META['CSRF_COOKIE']
     while not r.exists(sessionkey):
         print("waiting for session key")
+        checkvault(sessionkey)
     print("session key found")
-    
+
     try:
         tenant_entries = tenant.objects.all()
     except tenant.DoesNotExist:
