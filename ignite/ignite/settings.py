350b697486f8bdf4503c70a8667a8bde95b4c70e
Jakob Holst <jho@miracle.dk>
2024-02-09 20:50:20 +0100

    setting

--- ignite/ignite/settings.py
+++ ignite/ignite/settings.py
@@ -128,8 +128,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = 'static/'
+STATIC_URL = "https://ignite_static.openknowit.com/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
